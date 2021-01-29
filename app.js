2da22ca31cf9f99b9d7bab8e8b043279d79558a1
jashimgit <jashim.ncn@gmail.com>
2021-01-29 23:10:29 +0600

    ticket decrease button for first class working

--- app.js
+++ app.js
@@ -12,4 +12,14 @@ document.getElementById('fclass-increase-btn').addEventListener('click', functio
     // console.log(ticketIncreasedValue, ticketPrice);
 });
 
+document.getElementById('fclass-decrease-btn').addEventListener('click', function () {
+    // get input box value
+    var firstClassInputBtn = parseInt(document.getElementById('fclass-input-box').value);
+    // new ticket value after decrease by 1
+    let ticketIncreasedValue = firstClassInputBtn - 1;
+    let newTicketCount = document.getElementById('fclass-input-box').value = ticketIncreasedValue;
+    // count total ticket price
+    let ticketPrice = newTicketCount * 150;
+    console.log(ticketIncreasedValue, ticketPrice);
+})
 // console.log(firstClassInputBtn);
