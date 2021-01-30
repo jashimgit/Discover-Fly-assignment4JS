44533616f1243da58261173b29b8fa7aa1cb1ed2
jashimgit <jashim.ncn@gmail.com>
2021-01-30 15:15:50 +0600

    subtotal is working now

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-
 function updatePrice(isIncrease) {
 
-    var firstClassInputBtn = parseInt(document.getElementById('fclass-input-box').value);
+    var firstClassInputBtn = parseInt(document.getElementById('firstclass-count').value);
 
-    let ticketIncreasedValue = 0;
+    let ticketIncreasedValue;
 
     if (isIncrease == true) {
         ticketIncreasedValue = firstClassInputBtn + 1;
     }
     if (isIncrease == false) {
         if (firstClassInputBtn <= 0) {
-            document.getElementById('fclass-input-box').value = 0;
+            document.getElementById('firstclass-count').value = 0;
             ticketIncreasedValue = 0;
         } else {
 
@@ -18,10 +17,11 @@ function updatePrice(isIncrease) {
         }
     }
     // console.log(ticketIncreasedValue);
-    let newTicketCount = document.getElementById('fclass-input-box').value = ticketIncreasedValue;
+    let newTicketCount = document.getElementById('firstclass-count').value = ticketIncreasedValue;
     // count total ticket price
     let ticketPrice = newTicketCount * 150;
-    console.log(ticketIncreasedValue, ticketPrice);
+    // console.log(ticketIncreasedValue, ticketPrice);
+    totalPrice()
 }
 
 
@@ -47,7 +47,21 @@ function updateEconomyPrice(isIncrease) {
     let newTicketCount = document.getElementById('echo-class-input').value = ticketIncresedInput;
     // count total ticket price
     let ticketIncreasedValue = newTicketCount * 100;
-    console.log(newTicketCount, ticketIncreasedValue);
+    // console.log(newTicketCount, ticketIncreasedValue);
+    totalPrice()
 }
 
+// calculate total 
 
+function totalPrice() {
+
+    var firstclassCount = document.getElementById('firstclass-count');
+    var firstClassInput = parseInt(firstclassCount.value);
+    // economoy class ticket 
+    var economoyTicketCount = document.getElementById('echo-class-input');
+    var economyInput = parseInt(economoyTicketCount.value);
+    firstClassInput + economyInput;
+    var totalPrice = firstClassInput * 150 + economyInput * 100;
+    // console.log(totalPrice);
+    document.getElementById('sub-total').innerText = totalPrice;
+}
