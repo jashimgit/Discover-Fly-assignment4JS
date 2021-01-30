ba703ac1869f913dcb09a043394ac7a336936165
jashimgit <jashim.ncn@gmail.com>
2021-01-30 17:39:13 +0600

    all code refactored with optimized code and function

--- app.js
+++ app.js
@@ -1,110 +1,51 @@
 //  testing code 
-
-function updatePrice(isIncrease) {
-
-    var firstClassInputBtn = parseInt(document.getElementById('firstclass-count').value);
-
-    let ticketIncreasedValue;
-
+function handleProductChange(product, isIncrease) {
+    // get input value
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
     if (isIncrease == true) {
-        ticketIncreasedValue = firstClassInputBtn + 1;
+        productNewCount = productCount + 1;
     }
-    if (isIncrease == false) {
-        if (firstClassInputBtn <= 0) {
-            document.getElementById('firstclass-count').value = 0;
-            ticketIncreasedValue = 0;
-        } else {
-
-            ticketIncreasedValue = firstClassInputBtn - 1;
-        }
+    if (isIncrease == false && productCount > 0) {
+        productNewCount = productCount - 1;
     }
-    let newTicketCount = document.getElementById('firstclass-count').value = ticketIncreasedValue;
+    productInput.value = productNewCount;
 
-    let ticketPrice = newTicketCount * 150;
+    let fclassTotal;
+    if (product == 'firstclass') {
+        fclassTotal = productNewCount * 150
+    }
+    if (product == 'echoclass') {
+        fclassTotal = productNewCount * 100
+    }
 
     totalPrice()
 }
 
 
 
-
-
-
-
-// Working code 
-
-function updatePrice(isIncrease) {
-
-    var firstClassInputBtn = parseInt(document.getElementById('firstclass-count').value);
-
-    let ticketIncreasedValue;
-
-    if (isIncrease == true) {
-        ticketIncreasedValue = firstClassInputBtn + 1;
-    }
-    if (isIncrease == false) {
-        if (firstClassInputBtn <= 0) {
-            document.getElementById('firstclass-count').value = 0;
-            ticketIncreasedValue = 0;
-        } else {
-
-            ticketIncreasedValue = firstClassInputBtn - 1;
-        }
-    }
-    let newTicketCount = document.getElementById('firstclass-count').value = ticketIncreasedValue;
-
-    let ticketPrice = newTicketCount * 150;
-
-    totalPrice()
-}
-
-
-
-function updateEconomyPrice(isIncrease) {
-
-    var ecoClassInput = parseInt(document.getElementById('echoClass-count').value)
-
-    let ticketIncresedInput = 0;
-
-    if (isIncrease == true) {
-        ticketIncresedInput = ecoClassInput + 1;
-    }
-    if (isIncrease == false) {
-        if (ecoClassInput <= 0) {
-            document.getElementById('echoClass-count').value = 0;
-            ticketIncresedInput = 0;
-        } else {
-            ticketIncresedInput = ecoClassInput - 1;
-        }
-    }
-
-    let newTicketCount = document.getElementById('echoClass-count').value = ticketIncresedInput;
-
-    let ticketIncreasedValue = newTicketCount * 100;
-
-    totalPrice()
-}
-
-// calculate total Price
+// // calculate total Price
 
 function totalPrice() {
 
     var firstclassCount = document.getElementById('firstclass-count');
     var firstClassInput = parseInt(firstclassCount.value);
 
-    var economoyTicketCount = document.getElementById('echoClass-count');
+    var economoyTicketCount = document.getElementById('echoclass-count');
     var economyInput = parseInt(economoyTicketCount.value);
-    firstClassInput + economyInput;
+
+    // firstClassInput + economyInput;
     var totalPrice = firstClassInput * 150 + economyInput * 100;
 
     document.getElementById('sub-total').innerText = totalPrice;
 
-    // calculate vat 
+    // calculate vat
     let vat = parseInt(document.getElementById('vat').innerText);
     let totalVat = totalPrice * 0.1;
-    document.getElementById('vat').innerText = totalVat
+    document.getElementById('vat').innerText = totalVat;
 
-    // grand total 
+    // grand total
 
     document.getElementById('total-price').innerText = totalPrice + totalVat;
 }
@@ -113,7 +54,7 @@ function totalPrice() {
 
 function bookNow() {
     var totalFirstClassTicket = parseInt(document.getElementById('firstclass-count').value);
-    var totalEconomoTicket = parseInt(document.getElementById('echoClass-count').value);
+    var totalEconomoTicket = parseInt(document.getElementById('echoclass-count').value);
     let totalTicket = totalFirstClassTicket + totalEconomoTicket;
     var totalPrice = parseInt(document.getElementById('total-price').innerText);
     if (totalPrice > 0) {
